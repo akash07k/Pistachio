f2ec440927509825f51a79c02a231eaa4fd705f2
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2024-01-12 02:19:08 +0530

    feat(app): add the project link and stars in the header

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <>
       <header>
+        <a href="https://github.com/akash07k/Pistachio" target="_blank" rel="noopener noreferrer">
+          <img src="https://img.shields.io/github/stars/akash07k/Pistachio?style=social" alt="GitHub Repo stars" />
+        </a>
       </header>
     </>
   )
