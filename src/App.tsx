3e492c3e1f4452018ebfc90a85d634790ebed575
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2024-01-12 16:53:01 +0530

    style: reformat the code according to the defined Prettier rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <header>
-        <a href="https://github.com/akash07k/Pistachio" target="_blank" rel="noopener noreferrer">
-          <img src="https://img.shields.io/github/stars/akash07k/Pistachio?style=social" alt="GitHub Repo stars" />
+        <a
+          href="https://github.com/akash07k/Pistachio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://img.shields.io/github/stars/akash07k/Pistachio?style=social"
+            alt="GitHub Repo stars"
+          />
         </a>
       </header>
-      <main>
-      </main>
+      <main></main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
